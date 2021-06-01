4727e3ad653fe2cd99ada7b799d97f058fc044c0
Tomas Meskonis <Tomas.meskonis@gmail.com>
2021-06-01 16:31:58 +0100

    css work done

---
+++ script.js
@@ -0,0 +1,15 @@
+let operate = {
+
+plus: function (n1,n2) {
+    return n1 + n2
+},
+minus: function(n1,n2) {
+    return n1 - n2
+},
+multiply: function(n1,n2) {
+    return n1 * n2
+},
+divide: function(n1,n2) {
+    return n1 / n2
+},
+}
